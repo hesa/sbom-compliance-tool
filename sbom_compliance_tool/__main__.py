5f394a97718b32c0b6180b169ad87d94eede6212
Henrik Sandklef <hesa@sandklef.com>
2025-10-23 18:20:00 +0200

    read sbom file from user args

--- sbom_compliance_tool/__main__.py
+++ sbom_compliance_tool/__main__.py
@@ -26,8 +26,8 @@ def main():
 
     reader = SBoMReaderFactory.reader()
     logging.debug(f'Reader: {reader}')
-
-    report = reader.check_file('example-data/normalized-project.json',
+    
+    report = reader.check_file(args.sbom_file,
                                UseCase.usecase_to_string(UseCase.LIBRARY),
                                Provisioning.provisioning_to_string(Provisioning.BIN_DIST),
                                Modification.modification_to_string(Modification.UNMODIFIED))
@@ -44,6 +44,9 @@ def get_parser():
                                      epilog="",
                                      formatter_class=argparse.RawTextHelpFormatter)
 
+    parser.add_argument("sbom_file")
+    
+    
     parser.add_argument('-of', '--output-format',
                         type=str,
                         default='json')
