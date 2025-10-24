214988ea91d153d8f54bb9c35bbdbd25d7eaf7ea
Henrik Sandklef <hesa@sandklef.com>
2025-10-24 13:50:04 +0200

    add (c) and license info

--- sbom_compliance_tool/reader/cyclonedx.py
+++ sbom_compliance_tool/reader/cyclonedx.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import logging
 
 from sbom_compliance_tool.reader.sbom_reader import SBoMReader
--- sbom_compliance_tool/reader/sbom_reader.py
+++ sbom_compliance_tool/reader/sbom_reader.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 import json
 from xml.etree import ElementTree
 from enum import Enum
